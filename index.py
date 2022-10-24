7bad2455196bb1cf07f6063cea2441af8b9f4457
Joao Zsigmond <jzsiggy@gmail.com>
2022-10-23 22:11:33 -0300

    finito

--- index.py
+++ index.py
@@ -1,3 +1,4 @@
+from typing import Union
 from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
 from fastapi.encoders import jsonable_encoder
@@ -9,11 +10,11 @@ app = FastAPI()
 #######################################
 
 class Product(BaseModel):
-    id: int
-    name: str
-    description: str
-    price: float
-    quantity: int
+    id: Union [int, None] = None
+    name: Union [str, None] = None
+    description: Union [str, None] = None
+    price:  Union [float, None] = None
+    quantity:  Union [int, None] = None
 
     class Config:
         schema_extra = {
@@ -27,14 +28,12 @@ class Product(BaseModel):
         }
 
 class Inventory(BaseModel):
-    id: int
     product_id: int
     quantity: int
 
     class Config:
         schema_extra = {
             "example": {
-                "id": 1,
                 "product_id": 123,
                 "quantity": -4
             }
@@ -57,7 +56,10 @@ async def list_products():
     return products
 
 @app.post("/product/transaction")
-async def product_transaction(product_id: int, qty: int):
+async def product_transaction(movement: Inventory):
+    product_id = movement.product_id
+    qty = movement.quantity
+
     if (product_id in products):
         product = dict( products[product_id] )
 
@@ -79,9 +81,17 @@ async def create_product(product: Product):
         products[product.id] = product
         return product
 
-@app.put("/product")
-async def update_product(Product):
-    return {"message": "Hello World"}
+@app.patch("/product/{product_id}", response_model=Product)
+async def update_product(product_id: int, product: Product):
+    if (product_id in products):
+        stored_product_data = products[product_id]
+        stored_product_model = Product(**stored_product_data)
+        update_data = product.dict(exclude_unset=True)
+        updated_product = stored_product_model.copy(update=update_data)
+        products[product_id] = jsonable_encoder(updated_product)
+        return updated_product
+    else:
+        raise HTTPException(status_code=404, detail="product does not exist")
 
 @app.get("/product/{product_id}")
 async def get_product(product_id: int):
@@ -94,9 +104,10 @@ async def get_product(product_id: int):
 async def delete_product(product_id: int):
     if (product_id in products) :
         del products[product_id]
+        return products
     else :
         raise HTTPException(status_code=404, detail="product does not exist")
 
 @app.get("/")
 async def root():
-    return {"message": "Hello World"}
+    return {"message": "welcome to our inventory API"}
